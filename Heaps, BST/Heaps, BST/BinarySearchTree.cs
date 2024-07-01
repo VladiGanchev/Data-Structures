6aefa5887010dbd6d09ac726d16c4fbfdcbd2b95
VladiGanchev <160647367+VladiGanchev@users.noreply.github.com>
2024-07-01 17:05:43 +0300

    BST exercise

---
+++ Heaps, BST/Heaps, BST/BinarySearchTree.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heaps__BST
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public BinarySearchTree(Node<T> root = null)
+        {
+            Root = root;
+        }
+
+        public Node<T> Root { get; set; }
+
+        public void Insert(T value, Node<T> node)
+        {
+            if (node == null)
+            {
+                node = new Node<T>(value);
+                Root = node;
+                return;
+            }
+
+            if (node.Value.CompareTo(value) > 0)
+            {
+                if (node.RightChild == null)
+                {
+                    node.RightChild = new Node<T>(value);
+                    return;
+                }
+
+                Insert(value, node.RightChild);
+            }
+            else
+            {
+                if (node.LeftChild == null)
+                {
+                    node.LeftChild = new Node<T>(value);
+                    return;
+                }
+                Insert(value, node.LeftChild);
+            }
+
+        }
+
+        public bool Contains(T value, Node<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (node.Value.CompareTo(value) == 0)
+            {
+                return true;
+            }
+
+            if (node.Value.CompareTo(value) > 0)
+            {
+                return Contains(value, node.RightChild);
+            }
+            else
+            {
+                return Contains(value, node.LeftChild);
+            }
+        }
+
+        public Node<T> Search(T value, Node<T> node)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            if (node.Value.CompareTo(value) == 0)
+            {
+                return node;
+            }
+
+            if (node.Value.CompareTo(value) > 0)
+            {
+                return Search(value, node.RightChild);
+            }
+            else
+            {
+                return Search(value, node.LeftChild);
+            }
+        }
+
+        public string DFSInOrder(Node<T> node, int indent)
+        {
+
+            string result = "";
+
+            if (node.LeftChild != null)
+            {
+                result += DFSInOrder(node.LeftChild, indent + 2);
+            }
+
+            result += $"{new string(' ', indent)}{node.Value}\n";
+
+            if (node.RightChild != null)
+            {
+                result += DFSInOrder(node.RightChild, indent + 2);
+            }
+
+            return result;
+        }
+    }
+}
--- Heaps, BST/Heaps, BST/Program.cs
+++ Heaps, BST/Heaps, BST/Program.cs
@@ -65,5 +65,37 @@ public class Program
             Console.WriteLine($"Max element: {queue.Dequeue()}");
         }
 
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Binary Search Tree: ");
+        Console.WriteLine();
+
+
+        List<int> list = new List<int>();
+        for (int i = 0; i < 50; i+= 2)
+        {
+            list.Add(i);
+        }
+
+        BinarySearchTree<int> BStree = new BinarySearchTree<int>();
+        Insert(BStree, 0, list.Count, list);
+
+        Console.WriteLine($"Find: {43} {BStree.Contains(43, BStree.Root)}");
+
+        Console.WriteLine(BStree.DFSInOrder(BStree.Root, 0));
+
+    }
+
+    private static void Insert(BinarySearchTree<int> tree, int start, int end, List<int> list)
+    {
+        if (start >= end)
+        {
+            return;
+        }
+
+        var middle = (start + end) / 2;
+        tree.Insert(list[middle], tree.Root);
+        Insert(tree, start, middle - 1, list);
+        Insert(tree, middle + 1, end, list);
     }
 }
